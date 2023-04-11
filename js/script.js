55b7d27f1eae65fcc751243f476714266db446bd
valerie_l <contacttovalerie@gmail.com>
2023-04-11 00:26:21 -0400

    Added random image button

--- js/script.js
+++ js/script.js
@@ -9,6 +9,26 @@ const getImage = async function () {
     );
     const images = await res.json();
     console.log(images);
+    selectRandomImage(images);
 };
 
-getImage();
+const selectRandomImage = function (images) {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    const randomImage = images[randomIndex];
+    displayImage(randomImage);
+};
+
+const displayImage = function (randomImage) {
+    const author = randomImage.author;
+    const imageAddress = randomImage.download_url;
+    authorSpan.innerText = author;
+    img.src = imageAddress;
+    imgDiv.classList.remove("hide");
+};
+
+button.addEventListener ("click", function () {
+    getImage();
+});
+
+
+//getImage();
